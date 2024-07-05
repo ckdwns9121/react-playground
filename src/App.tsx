e7ade4437350b8bcd472cb3c613caa97d9f52201
ChangjunPark <ckdwns9121@naver.com>
2024-07-05 09:40:37 +0900

    # app

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,7 @@
 import "./App.css";
-import { useDragAndDrop } from "./hooks/useDragAndDrop";
-import DragableContext from "./components/dnd/Dragable";
-import DropContext from "./components/dnd/DropContext";
-import Item from "./components/item/Item";
-
-const initItem = [
-  { id: "1", name: "Item 1❤" },
-  { id: "2", name: "Item 2🧡" },
-  { id: "3", name: "Item 3💘" },
-  { id: "4", name: "Item 4❤" },
-  { id: "5", name: "Item 5🧡" },
-  { id: "6", name: "Item 6💘" },
-];
 
 function App() {
-  const { items, draggedItemIndex, handleDragStart, handleDragEnter, handleDragEnd } = useDragAndDrop(initItem);
-  return (
-    <div>
-      <h2>Drag and Drop Example</h2>
-      <div style={{ display: "flex", justifyContent: "space-between" }}>
-        <div>
-          <h3 draggable>Drag Items</h3>
-          {items.map((item, index) => (
-            <DragableContext
-              key={item.id}
-              dragabbleId={item.id}
-              index={index}
-              onItemDragStart={handleDragStart}
-              onItemDragEnter={handleDragEnter}
-              onItemDragEnd={handleDragEnd}
-              isDragging={draggedItemIndex === index}
-            >
-              <Item name={item.name} />
-            </DragableContext>
-          ))}
-        </div>
-        <DropContext dragItems={items} />
-      </div>
-    </div>
-  );
+  return <div>app</div>;
 }
 
 export default App;
